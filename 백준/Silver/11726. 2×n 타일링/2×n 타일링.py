9defa92ceccb7533c1b49d318e63adf1e4348fe9
여인범 <66177781+yesyes-12@users.noreply.github.com>
2026-03-23 17:43:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def tileling():
+    n = int(input())
+    
+    if n==1:
+        print(1)
+        return
+    
+    dp=[0]*(n+1)
+    dp[1]=1
+    dp[2]=2
+    
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        dp[i] %= 10007
+        
+    print(dp[n])
+tileling()
