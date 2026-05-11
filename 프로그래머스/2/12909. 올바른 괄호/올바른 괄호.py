5dda1891838c75c686d18e3e80d54e180ab052ac
여인범(Yeo In-Beom) <66177781+yesyes-12@users.noreply.github.com>
2026-05-11 13:16:07 +0900

    [level 2] Title: 올바른 괄호, Time: 7.37 ms, Memory: 9.31 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,10 @@
+def solution(s):
+    count=0
+    for i in s:
+        if i=="(":
+            count+=1
+        else:
+            count-=1
+        if count<0:
+            return False
+    return True if count==0 else False
